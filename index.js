ce1a6eef3401eb08781c77cccb1912f9bb803f50
mmwandera <mmwandera74@gmail.com>
2023-10-13 16:12:51 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Add your code here
+function submitData(name, email){
+    
+    // Test 4 - Return the Fetch Chain
+    return(
+
+        // Test 1 - Send Data
+        fetch(`http://localhost:3000/users`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                Accept: 'application/json'
+            },
+            body: JSON.stringify({
+                name: name,
+                email: email
+            })
+        })
+
+        // Test 2 - Handle the Response
+        .then(res => res.json())
+        .then(data => {
+            document.body.innerHTML += data.id
+        })
+
+        // Test 3 - Handle Errors
+        .catch(error => {
+            document.body.innerHTML += error.message
+        })
+    )
+}
+
+submitData('Mark', 'mmwandera74@gmail.com')
